82e6dee80c4e96009f26e87d29241ab10a5b810c
ProgNovel <prognovel@gmail.com>
2020-04-04 00:17:47 +0800

    add initial (still red) tests for custom syntax pointer pool

---
+++ src/fund/tests/template-scrape-custom-syntax.spec.ts
@@ -0,0 +1,63 @@
+import { fund, getCurrentPointerPool } from '../main'
+
+import { toBeInTheDocument, toHaveAttribute } from '@testing-library/jest-dom/matchers'
+import { scriptFundmeIsNotApplicationJson } from '../errors'
+
+expect.extend({ toBeInTheDocument, toHaveAttribute })
+
+describe('parsing custom syntax', () => {
+  test('works with hash # weight modifier', () => {
+    document.body.innerHTML = `
+      <template fundme>
+        $wallet.example.com/testing-one#22;
+        $wallet.example.com/testing-two#44;
+        $wallet.example.com/testing-three#33;
+      </template>
+    `
+    fund()
+    const expectedPool = [
+      {
+        address: '$wallet.example.com/testing-one',
+        weight: 22,
+      },
+      {
+        address: '$wallet.example.com/testing-two',
+        weight: 44,
+      },
+      {
+        address: '$wallet.example.com/testing-three',
+        weight: 33,
+      },
+    ]
+
+    expect(getCurrentPointerPool()).toEqual(expectedPool)
+    document.body.innerHTML = ''
+  })
+  test('works with a pointer with no weight explicitly stated', () => {
+    document.body.innerHTML = `
+      <template fundme>
+        $wallet.example.com/testing-one;
+        $wallet.example.com/testing-two#44;
+        $wallet.example.com/testing-three#33;
+      </template>
+    `
+    fund()
+    const expectedPool = [
+      {
+        address: '$wallet.example.com/testing-one',
+        weight: 5,
+      },
+      {
+        address: '$wallet.example.com/testing-two',
+        weight: 44,
+      },
+      {
+        address: '$wallet.example.com/testing-three',
+        weight: 33,
+      },
+    ]
+
+    expect(getCurrentPointerPool()).toEqual(expectedPool)
+    document.body.innerHTML = ''
+  })
+})
