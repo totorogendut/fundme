4f19618dcd8e00ba667ff228a1f5286bb6e7f550
ProgNovel <prognovel@gmail.com>
2020-06-07 18:11:12 +0800

    chore: set correct no undefined error message on server-side fund()

--- src/fund/fund-server.ts
+++ src/fund/fund-server.ts
@@ -1,10 +1,10 @@
 import { isMultiplePointer, setCurrentPointer } from "./utils";
 import { setPointerMultiple } from "./set-pointer-multiple";
-import { FundmeError, invalidFundmeServerSide } from "./errors";
+import { FundmeError, invalidFundmeServerSide, noUndefinedFundOnServerSide } from "./errors";
 import { setPointerSingle } from "./set-pointer-single";
 
 export function serverSideFund(pointer: WMAddress): string {
-  if (pointer === undefined) throw FundmeError(invalidFundmeServerSide);
+  if (pointer === undefined) throw FundmeError(noUndefinedFundOnServerSide);
 
   if (typeof pointer === "string") {
     return setPointerSingle(pointer).toString();
